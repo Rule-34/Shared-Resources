6de13b6e7a735336e778733ebc40bf2f0a711a2c
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-06-06 11:43:20 +0200

    chore: remove unnecessary comments

--- util/BooruUtils.js
+++ util/BooruUtils.js
@@ -1,23 +1,10 @@
-// Load boorus from JSON
 import booruList from '../booru-list.json';
 import booruTypeList from '../booru-type-list.json';
 
-/**
- * Returns an Array of filtered results
- * @param {boolean | string} value Value to compare
- * @param {string} key Key of the object
- * @param {array} list Array to compare results
- */
 function findBoorusWithValueByKey(value, key = 'domain', list = booruList) {
   return list.filter((booru) => booru[key] === value);
 }
 
-/**
- * Returns an Array of multiple filtered results
- * @param {boolean | string} valueArray Array of values to compare
- * @param {string} key Key of the object
- * @param {array} list Array to compare results
- */
 function removeBoorusWithValuesByKey(
   valueArray,
   key = 'domain',
